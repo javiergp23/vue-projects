73a3815cf59ed362786dd65c2a98465af53e8446
javiergp23 <javgon2306@gmail.com>
2025-06-02 15:46:10 -0300

    add crud

---
+++ crud/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Hello desde homeview</h1>
+</template>
--- split-bill/src/components/PeopleView.vue
+++ split-bill/src/components/PeopleView.vue
@@ -1,21 +1,32 @@
 <script setup>
 import { getGrandTotal, store } from '../stores/index';
+import Label from './Label.vue';
 </script>
 
 <template>
     <div class="no-items" v-if="store.people.length == 0">No Items</div>
     <div class="people-view" v-if="store.people.length > 0">
-        <div>
+        <div class="header">
+
             <div>
-                {{getGrandTotal()}}
+                <div>
+                    <Label title="Total + tip: " :value="getGrandTotal()"/>
+                    
+                </div>
             </div>
-        </div>
-        <div>
             <div>
-                {{store.params.remaining}}
+                <div>
+                    <Label title="Remaining: " :value="store.params.remaining"/>
+                </div>
+            </div>
+            <div class="people-container">
+                <div v-for="person in store.people" :key="person.id">
+                    {{person.totalPerPerson}}
+                </div>
             </div>
         </div>
     </div>
+
 </template>
 
 <style scoped>
