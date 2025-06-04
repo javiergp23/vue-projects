badcc571dfc561832e562d763f402d64cce74ed8
javiergp23 <javgon2306@gmail.com>
2025-06-04 15:19:06 -0300

    create router/composition

--- vue-crud-composition/src/App.vue
+++ vue-crud-composition/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld msg="Vite + Vue" />
+  <RouterView/>
 </template>
 
 <style scoped>
---
+++ vue-crud-composition/src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>DEsde Home</h1>
+</template>
