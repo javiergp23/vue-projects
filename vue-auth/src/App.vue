0588a8d4e0b32293244f27238244f6a311cd7a3c
javiergp23 <javgon2306@gmail.com>
2025-06-10 12:59:30 -0300

    create Home/vue-auth

--- vue-auth/src/App.vue
+++ vue-auth/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
 import { RouterView } from 'vue-router'
+import Home from './views/HomeView.vue'
 </script>
 
 <template>
   <div class="wrapper">
     <NavBar/>
+    <Home/>
     <RouterView/>
   </div>
 </template>
