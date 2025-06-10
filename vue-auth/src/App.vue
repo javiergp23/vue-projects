2b6d4e821f016a5355950ab64afbcde62f7838e0
javiergp23 <javgon2306@gmail.com>
2025-06-09 21:16:10 -0300

    create components/vue-auth

--- vue-auth/src/App.vue
+++ vue-auth/src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavBar from './components/NavBar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <div class="wrapper">
     <NavBar/>
+    <RouterView/>
   </div>
 </template>
 
