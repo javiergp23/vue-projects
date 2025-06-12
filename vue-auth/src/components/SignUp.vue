8345a884ebf760e1f06158cef68622470c0118f7
javiergp23 <javgon2306@gmail.com>
2025-06-12 16:09:44 -0300

    fix form/signUp

--- vue-auth/src/components/SignUp.vue
+++ vue-auth/src/components/SignUp.vue
@@ -1,29 +1,41 @@
 <script setup>
+import { reactive } from 'vue';
 
+const user = reactive({
+    name: '',
+    lastname: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+})
+
+const handleSubmit = async () => {
+    console.log(user)
+}
 </script>
 
 <template>
-    <form action="" class="container mx-auto flex flex-col justify-center  border rounded-md p-4 shadow-lg w-[400px] mt-10">
+    <form @submit.prevent="handleSubmit" class="container mx-auto flex flex-col justify-center  border rounded-md p-4 shadow-lg w-[400px] mt-10">
         <h3 class="text-center text-2xl font-bold">Sign Up</h3>
         <div class="flex flex-col p-2">
             <label for="name">Name</label>
-            <input class="border rounded-xl p-2 m-2" name="name" type="text" placeholder="name">
+            <input class="border rounded-xl p-2 m-2" name="name" type="text" placeholder="name" v-model="user.name" required>
         </div>
         <div class="flex flex-col p-2">
             <label for="lastname">lastname</label>
-            <input class="border rounded-xl p-2 m-2" name="lastname" type="text" placeholder="lastname">
+            <input class="border rounded-xl p-2 m-2" name="lastname" type="text" placeholder="lastname" v-model="user.lastname" required>
         </div>
         <div class="flex flex-col p-2">
             <label for="email">Email</label>
-            <input class="border rounded-xl p-2 m-2" name="email" type="email" placeholder="email">
+            <input class="border rounded-xl p-2 m-2" name="email" type="email" placeholder="email" v-model="user.email" required>
         </div>
         <div class="flex flex-col p-2">
             <label for="password">Password</label>
-            <input class="border rounded-xl p-2 m-2" name="password" type="password" placeholder="Password">
+            <input class="border rounded-xl p-2 m-2" name="password" type="password" placeholder="Password" v-model="user.password" required>
         </div>
         <div class="flex flex-col p-2">
-            <label for="password">Confirm Password</label>
-            <input class="border rounded-xl p-2 m-2" name="password" type="password" placeholder="Confirm Password">
+            <label for="confirmPassword">Confirm Password</label>
+            <input class="border rounded-xl p-2 m-2" name="Password" type="password" placeholder="Confirm Password" v-model="user.confirmPassword" required>
         </div>
         <div class="flex justify-center">
             <button class="bg-blue-500 cursor-pointer text-white hover:text-black hover:bg-blue-100 hover:shadow-lg transition-all duration-300 ease-in-out hover:font-bold rounded-xl p-2 m-2 w-[120px]" type="submit">
@@ -31,4 +43,4 @@
             </button>
         </div>
     </form>
-</template>
+</template>user.name
