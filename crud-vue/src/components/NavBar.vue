0840d77bd723a4ef5c933e96f516fc743d833318
javiergp23 <javgon2306@gmail.com>
2025-06-25 11:24:05 -0300

    create: views/crud-vue

---
+++ crud-vue/src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1>CRUD-VUE</h1>
+    <RouterLink to="/">Listado</RouterLink>
+    <RouterLink to="/create">Crear nuevo usuario</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
